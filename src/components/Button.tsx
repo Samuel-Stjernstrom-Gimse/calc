9777633481efdae3a0f6e25a778742de293921bf
Zam <samuel.kodehode@gmail.com>
2024-03-22 23:58:25 +0100

    1.0.1

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,11 @@ interface Props {
 }
 
 export const Button = (props: Props) => {
-	let buttonSymbol = props.sym
+	let buttonSymbol: string = props.sym
 	if (props.sym === 'Math.sqrt(') buttonSymbol = '√'
+	if (props.sym === 'Math.cos(') buttonSymbol = 'cos'
+	if (props.sym === 'Math.sin(') buttonSymbol = 'sin'
+	if (props.sym === 'Math.tan(') buttonSymbol = 'tan'
 	if (props.sym === '/100') buttonSymbol = '%'
 
 	return (
@@ -24,7 +27,8 @@ export const Button = (props: Props) => {
 					display: 'flex',
 					border: '2px solid black',
 					borderRadius: '0.5rem',
-					fontSize: '9px'
+					fontSize: '9px',
+					cursor: 'pointer'
 				}}
 				onClick={() => props.handleInput(props.sym)}
 			>
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -3,46 +3,95 @@ import { useState } from 'react'
 import { Screen } from './Screen.tsx'
 
 export const Calculator = () => {
-	const numberArray: string[] = ['(', ')', 'del', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.', '=']
-	const operatorsArray: string[] = ['/', '*', '-', '+', 'Math.sin(', 'Math.cos(', 'Math.tan(', 'Math.sqrt(', '/100']
+	const numberArray: string[] = [
+		'7',
+		'8',
+		'9',
+		'del',
+		'4',
+		'5',
+		'6',
+		'+',
+		'1',
+		'2',
+		'3',
+		'-',
+		'0',
+		'.',
+		'=',
+		'(',
+		')',
+		'/',
+		'*',
+		'Math.sin(',
+		'Math.cos(',
+		'Math.tan(',
+		'Math.sqrt(',
+		'/100'
+	]
+
 	const [sum, setSum] = useState<string>('')
-	const [calculation, setCalculation] = useState<string>('')
+	const [calculation, setCalculation] = useState<string[]>([])
 	const [drawScreen, setDrawScreen] = useState<string[]>([])
 
 	const handleNumberArray = (inputNumber: string): void => {
+		const screenInput: string =
+			inputNumber === 'Math.cos('
+				? 'cos('
+				: inputNumber === 'Math.sin('
+					? 'sin('
+					: inputNumber === 'Math.tan('
+						? 'tan('
+						: inputNumber === 'Math.sqrt('
+							? '√('
+							: inputNumber === '/100'
+								? '%'
+								: inputNumber === 'del'
+									? ' '
+									: inputNumber
+
 		if (inputNumber === '=') {
-			setSum(eval(calculation + inputNumber))
+			setSum(eval(calculation.join('')))
 		} else if (inputNumber === 'del') {
-			setCalculation((current: string) => {
-				return current.slice(0, -1)
+			setCalculation((current: string[]) => {
+				if (calculation.length === 1) {
+					return [' ']
+				} else {
+					return current.slice(0, -1)
+				}
 			})
-			setSum(eval(calculation.slice(0, -1)))
+
+			setDrawScreen((current: string[]): [string] | string[] => {
+				if (drawScreen.length === 1) {
+					return [' ']
+				} else {
+					return current.slice(0, -1)
+				}
+			})
+
+			setSum(eval(calculation.slice(0, -1).join('')))
 		} else {
-			setCalculation((current: string) => current + inputNumber)
-			setSum(eval(calculation + inputNumber))
+			setDrawScreen((prevState: string[]) => [...prevState, screenInput])
+			setCalculation((current: string[]) => [...current, inputNumber])
+			setSum(eval(calculation.join('') + inputNumber))
 		}
 	}
 
 	return (
 		<>
 			<Screen showScreen={sum} />
-			<Screen showScreen={calculation} />
+			<Screen showScreen={drawScreen.join('')} />
 			<div style={{ display: 'flex' }}>
 				<div
 					style={{
 						display: 'grid',
-						gridTemplateColumns: 'repeat(3, 1fr)',
+						gridTemplateColumns: 'repeat(4, 1fr)',
 						gridGap: '1px',
 						width: '200px'
 					}}
 				>
 					{numberArray.map((numberInput: string) => {
 						return <Button handleInput={handleNumberArray} sym={numberInput} />
-					})}
-				</div>
-				<div style={{ display: 'flex', flexDirection: 'column' }}>
-					{operatorsArray.map((operator: string) => {
-						return <Button handleInput={handleNumberArray} sym={operator} />
 					})}
 				</div>
 			</div>
